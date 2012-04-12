48750a269f92fc3e7a7d550e8351a142825ef58e
Rob Taylor <manix84@gmail.com>
2012-04-12 21:43:27 +0100

    Cleaning up logging.
    Removing code I was playing with, but didn't intend to enter master.

--- script/logging.js
+++ script/logging.js
@@ -6,62 +6,51 @@ define('utils/logging', [
     'utils/logging/popup'
 ], function (loggingPopup) {
 
-    (function () {
-        var i = 0,
-            loggingMethods = [
-                "log",
-                "debug",
-                "info",
-                "warn",
-                "error",
-                "assert",
-                "clear",
-                "dir",
-                "dirxml",
-                "trace",
-                "group",
-                "groupCollapsed",
-                "groupEnd",
-                "time",
-                "timeEnd",
-                "profile",
-                "profileEnd",
-                "count",
-                "exception",
-                "table"
-            ],
-            emptyFunc = function () {};
+    var i = 0,
+        loggingMethods = [
+            "log",
+            "debug",
+            "info",
+            "warn",
+            "error",
+            "assert",
+            "clear",
+            "dir",
+            "dirxml",
+            "trace",
+            "group",
+            "groupCollapsed",
+            "groupEnd",
+            "time",
+            "timeEnd",
+            "profile",
+            "profileEnd",
+            "count",
+            "exception",
+            "table"
+        ],
+        emptyFunc = function () {};
 
-        if (!enableDebug) {
-            for (; i < loggingMethods.length; i++) {
-                window.console[loggingMethods[i]] = emptyFunc;
-            }
-        } else if (!!window.console) {
-            window.onerror = function (errorMessage, fileName, lineNumber) {
-                loggingPopup.addMessage('error', [
-                    'Error: ' + errorMessage,
-                    'File: ' + fileName,
-                    'Line: ' + lineNumber
-                ]);
-                return false;
+    if (!enableDebug) {
+        for (; i < loggingMethods.length; i++) {
+            window.console[loggingMethods[i]] = emptyFunc;
+        }
+    } else if (!!window.console) {
+        window.onerror = function (errorMessage, fileName, lineNumber) {
+            loggingPopup.addMessage('error', [
+                'Error: ' + errorMessage,
+                'File: ' + fileName,
+                'Line: ' + lineNumber
+            ]);
+            return false;
+        };
+
+        window.console = {};
+        for (; i < loggingMethods.length; i++) {
+            window.console[loggingMethods[i]] = function () {
+                loggingPopup.addMessage(loggingMethods[i], arguments);
             };
-
-            window.console = {};
-            for (; i < loggingMethods.length; i++) {
-                window.console[loggingMethods[i]] = function () {
-                    loggingPopup.addMessage(loggingMethods[i], arguments);
-                };
-            }
         }
-    }());
-
-    var logging = {
-        init: function () {
-        }
-    };
-
-    logging.init();
-
-    return logging;
+    }
 
 });
