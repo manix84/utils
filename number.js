dba709122d1ccfe7473463c630579b6965cf3376
Rob Taylor <manix84@gmail.com>
2012-05-09 07:32:46 +0100

    Adding round to decimal method to number.

--- number.js
+++ number.js
@@ -23,6 +23,16 @@ define('utils/number', function () {
                 input = "0" + input;
             }
             return input;
+        },
+
+        /**
+         * Rounds a float to a decimal place, rather than removing the decimal all together.
+         * @param  {Number} number - A float that needs rounding.
+         * @param  {Number} decimal - The number of decimal places that you want to keep.
+         * @return {Number} A float with the corrisponding number of decimal places.
+         */
+        round: function (number, decimal) {
+            return Math.round(number * Math.pow(10, decimal)) / Math.pow(10, decimal);
         }
     };
 
