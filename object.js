5e815c406db66c022ce3f029ca695188fb67ca95
Rob Taylor <manix84@gmail.com>
2012-06-24 16:12:06 +0100

    Separating isObject out of object.

--- object.js
+++ object.js
@@ -12,16 +12,6 @@ define('utils/object', [
      * @requires utils/core
      */
     var object = {
-        /**
-         * Checks whether argument passed is an object
-         * @param {mixed} object - Item to be tested
-         * @example object.isObject({key1: 'var', key2: 'var'}); // RETURNS: true
-         * @example object.isObject(['key1', 'key2']); // RETURNS: false
-         * @returns {boolean}
-         */
-        isObject: function (object) {
-            return utilsBase.getType(object) === 'object';
-        },
 
         /**
          * Checks the length of the passed object
