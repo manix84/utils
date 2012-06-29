91657964168f6dd6d19e28eb2211b526bb59fb6e
Rob Taylor <manix84@gmail.com>
2012-06-29 18:08:31 +0100

    Separating pad from number util.

--- number.js
+++ number.js
@@ -9,22 +9,6 @@ define('utils/number', function () {
      */
     var number = {
 
-         /**
-          * Adds a repeating string to the start of a number. The number is converted to a string to accomplish this.
-          * @param  {[type]} input - Number to be padded.
-          * @param  {[type]} [padLength] - The length you want the number to be outputted at. Defaults to "2"
-          * @return {string} (EG: "01")
-          */
-        pad: function (input, padLength) {
-            input = String(input);
-            padLength = padLength || 2;
-
-            while (input.length < padLength) {
-                input = "0" + input;
-            }
-            return input;
-        },
-
         /**
          * Rounds a float to a decimal place, rather than removing the decimal all together.
          * @param  {Number} number - A float that needs rounding.
