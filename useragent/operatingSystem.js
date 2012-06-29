6fba5f229a1b57937828e3785f08506f523d1b47
Rob Taylor <manix84@gmail.com>
2012-06-29 18:36:07 +0100

    Adding missed window.navigation.useragent to operatingSystem.js

--- useragent/operatingSystem.js
+++ useragent/operatingSystem.js
@@ -11,25 +11,27 @@ define('utils/useragent/operatingSystem', function () {
      * @property os.ios {boolean|string}
      */
     return (function () {
-        var operatingSystem = {
-            win: /windows/.test(ua),
-            mac: /mac/.test(ua),
-            linux: /linux/.test(ua),
-            unix: /x11/.test(ua),
-            ios: /like mac os x/.test(ua)
-        };
+        var useragent = window.navigator.userAgent.toLowerCase(),
+            operatingSystem = {
+                win: /windows/.test(useragent),
+                mac: /mac/.test(useragent),
+                linux: /linux/.test(useragent),
+                unix: /x11/.test(useragent),
+                ios: /like mac os x/.test(useragent)
+            };
+
         if (operatingSystem.win) {
-            if (/windows/.test(ua) && /nt 5\./.test(ua)) {
+            if (/windows/.test(useragent) && /nt 5\./.test(useragent)) {
                 operatingSystem.win = 'xp';
-            } else if (/windows/.test(ua) && /nt 6\.0/.test(ua)) {
+            } else if (/windows/.test(useragent) && /nt 6\.0/.test(useragent)) {
                 operatingSystem.win = 'vista';
-            } else if (/windows/.test(ua) && /nt 6\.1/.test(ua)) {
+            } else if (/windows/.test(useragent) && /nt 6\.1/.test(useragent)) {
                 operatingSystem.win = '7';
             }
         } else if (operatingSystem.ios) {
-            if (/like mac os x/.test(ua) && /os 4/.test(ua)) {
+            if (/like mac os x/.test(useragent) && /os 4/.test(useragent)) {
                 operatingSystem.ios = '4';
-            } else if (/like mac os x/.test(ua) && /os 5/.test(ua)) {
+            } else if (/like mac os x/.test(useragent) && /os 5/.test(useragent)) {
                 operatingSystem.ios = '5';
             }
         }
