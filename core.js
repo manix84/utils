573e984d69e7d6ea4927bf27889513a00508114c
Rob Taylor <manix84@gmail.com>
2012-06-29 10:34:30 +0100

    Separating getScripts into it's own scripts module sections.

--- core.js
+++ core.js
@@ -19,38 +19,6 @@ define('utils/core', function () {
         _rscript: '<script[^>]*>(?!\\s*<\\/script>)([\\s\\S]*?)<\\/script>',
 
         /**
-         * Takes a string and parses it for all inline script tags. It returns an array of all the
-         * scripts found and removes them from the string. If no scripts are found, it returns an empty array.
-         * @params {string} string - The string that is supposed to be parsed
-         * @returns {Array} Array of scripts to be evaled. If no scripts are found, it returns
-         *          an empty array.
-         */
-        getScripts: function (string) {
-            // regular expression that searches for script tags and gets the content
-            // this regular expression is only supposed to match script tags that contain inline scripts
-            //var rscript = /<script[^>]*>(?!\s*<\/script>)([\s\S]*?)<\/script>/i,
-            // NB: FOR WHATEVER REASON, THE REGULAR EXPRESSION ABOVE WORKS, WHEREAS IF WE CONSTRUCT
-            // IT LIKE THIS, IT DOESN'T... ELSE WE COULD USE THE PROPERTY AS IS USED IN THE REMOVEINLINESCRIPT
-            // TO CONSTRUCT OUR REGEX
-            var rscript = new RegExp(this._rscript, 'i'),
-                scripts = [],
-                results;
-
-            do {
-                results = rscript.exec(string);
-                // save the script
-                if (results) {
-                    if (results[1]) {
-                        scripts.push(results[1]);
-                    }
-                }
-                string = string.replace(rscript, '');
-            } while (results);
-
-            return scripts;
-        },
-
-        /**
          * Remove all inline scripts from a html string.
          * @params {string} string - A string of html which may or may not contain a script tag.
          * @returns {string} returns the html without any script tags.
