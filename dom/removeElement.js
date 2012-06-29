a649ce20ec976d2c3367a83fd70f7178892b27c6
Rob Taylor <manix84@gmail.com>
2012-06-29 16:17:41 +0100

    Adding Dom removal util.

---
+++ dom/removeElement.js
@@ -0,0 +1,19 @@
+/**
+ * @author Rob Taylor [manix84@gmail.com]
+ */
+define('utils/dom/removeElement', function () {
+
+
+    var trash = document.createElement("div"),
+        /**
+         * Remove element from the dom.
+         * @exports utils/dom/removeElement
+         * @param  {String} elementId - Dom element ID.
+         */
+        removeElement = function (elementId) {
+            trash.appendChild(document.getElementById(elementId));
+            trash.innerHTML = '';
+        };
+
+    return removeElement;
+});
