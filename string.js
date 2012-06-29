81d53ef1575545029775867d6ad78f1b8af43ae1
Rob Taylor <manix84@gmail.com>
2012-06-29 17:53:26 +0100

    Separating wordCount from string util.

--- string.js
+++ string.js
@@ -15,43 +15,6 @@ define('utils/string', [
     var string = {
 
         /**
-         * Counts the number of words inside string. If the optional format is not specified, then the return value will
-         * be an integer representing the number of words found. In the event the format is specified, the return value
-         * will be an array, content of which is dependent on the format. The possible value for the format and the
-         * resultant outputs are listed below.
-         * @param {string} string - The String
-         * @param {number} [format] - Specify the return value of this function. The current supported values are:
-         * <!-- Sorry, this the code below is for JSDocs v3 -->
-         * <table class="props">
-         *     <thead>
-         *         <tr>
-         *             <th>Value</th>
-         *             <th>Description</th>
-         *         </tr>
-         *     </thead>
-         *     <tbody>
-         *         <tr>
-         *             <td class="name"><code>0</code></td>
-         *             <td class="description">Returns the number of words found (default)</td>
-         *         </tr>
-         *         <tr>
-         *             <td class="name"><code>1</code></td>
-         *             <td class="description">Returns an array containing all the words found inside the string</td>
-         *         </tr>
-         *     </tbody>
-         * </table>
-         * @returns {number|array}
-         */
-        wordCount: function (string, format) {
-            switch (format) {
-            case 1: case 2:
-                return string.split(' ');
-            default: // case 0:
-                return string.split(' ').length;
-            }
-        },
-
-        /**
          * Capitalise the first letter of the string.
          * @param {string} string - String to be capitalised
          * @returns {string}
