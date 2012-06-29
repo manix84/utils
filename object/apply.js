e2a0ced063810a59652cecee43bbc86651d761ce
Rob Taylor <manix84@gmail.com>
2012-06-30 00:09:07 +0100

    Adding object.apply and object.extend

---
+++ object/apply.js
@@ -0,0 +1,25 @@
+/**
+ * @author Rob Taylor [manix84@gmail.com]
+ */
+define('utils/object/apply', function () {
+    /**
+     * Copies properties from one object to another. All properties from 'source' will be copied onto 'destination',
+     * potentially overwriting existing properties on 'destination'. Properties from 'source's prototype chain will not
+     * be copied.
+     * @param {Object} destination - Destination object. If this object is undefined or falsey, a new object will be
+     *     created.
+     * @param {Object} source - Properties of this object will be copied onto the destination. If this object is
+     *     undefined or falsey, a new object will be created.
+     * @returns {Object}
+     */
+    var apply = function (source, destination) {
+        var property;
+        for (property in source) {
+            if (source.hasOwnProperty(property)) {
+                destination[property] = source[property];
+            }
+        }
+    };
+
+    return apply;
+});
