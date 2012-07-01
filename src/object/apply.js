da5fbdf92e7ba205d3d321df24ed3e836fc0ad2b
Rob Taylor <manix84@gmail.com>
2012-07-01 16:58:32 +0100

    Relocating object/apply references.

--- src/object/apply.js
+++ src/object/apply.js
@@ -1,11 +1,13 @@
 /**
  * @author Rob Taylor [manix84@gmail.com]
  */
-define('utils/object/apply', function () {
+define('object/apply', function () {
     /**
      * Copies properties from one object to another. All properties from 'source' will be copied onto 'destination',
      * potentially overwriting existing properties on 'destination'. Properties from 'source's prototype chain will not
      * be copied.
+     * @exports object/apply
+     *
      * @param {Object} destination - Destination object. If this object is undefined or falsey, a new object will be
      *     created.
      * @param {Object} source - Properties of this object will be copied onto the destination. If this object is
