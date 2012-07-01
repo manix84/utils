694b79bdb728f0db3a49a39aabd8203ba362fb25
Rob Taylor <manix84@gmail.com>
2012-07-01 16:57:18 +0100

    Relocating number/round references.

--- src/number/round.js
+++ src/number/round.js
@@ -1,10 +1,12 @@
 /**
  * @author Rob Taylor [manix84@gmail.com]
  */
-define('utils/number/round', function () {
+define('number/round', function () {
 
     /**
      * Rounds a float to a decimal place, rather than removing the decimal all together.
+     * @exports number/round
+     *
      * @param  {Number} number - A float that needs rounding.
      * @param  {Number} decimal - The number of decimal places that you want to keep.
      * @return {Number} A float with the corrisponding number of decimal places.
