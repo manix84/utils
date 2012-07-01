d867bffe693ae9e6fd853ea7d90802b187fd3509
Rob Taylor <manix84@gmail.com>
2012-07-01 16:53:59 +0100

    Relocating events/addListener references.

--- src/events/addListener.js
+++ src/events/addListener.js
@@ -1,10 +1,12 @@
 /**
  * @author Rob Taylor [manix84@gmail.com]
  */
-define('utils/events/addListener', function () {
+define('events/addListener', function () {
 
     /**
      * Adds an event listener to a DOM Element.
+     * @exports events/addListener
+     *
      * @param {HTMLElement} element - The object to attach the event listener to.
      * @param {String} name - The event name.
      * @param {Function} callback - The function to be called when the event fires.
