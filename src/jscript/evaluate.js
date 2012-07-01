0f2da545d18f6185b438eded0310d2272b710a57
Rob Taylor <manix84@gmail.com>
2012-07-01 16:55:07 +0100

    Relocating jscript/evaluate references.

--- src/jscript/evaluate.js
+++ src/jscript/evaluate.js
@@ -2,9 +2,11 @@
  * @author Rob Taylor [manix84@gmail.com]
  */
 
-define('utils/jscript/evaluate', function () {
+define('jscript/evaluate', function () {
     /**
      * Evals an array of scripts. This is a companion function to be used with the getScripts function.
+     * @exports jscript/evaluate
+     *
      * @params {array} scripts - An array of script strings to be evaled
      * @returns {array} An array of return values from the individual scripts
      */
