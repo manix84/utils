722a083e5ada6faf3c5f419c4f456f6a1f6452d9
Rob Taylor <manix84@gmail.com>
2012-07-01 22:20:27 +0100

    Adding keyboard map test.

---
+++ tests/keyboard/map.js
@@ -0,0 +1,36 @@
+/**
+ * Tests for keyboard/map
+ * @author Rob Taylor [manix84@gmail.com]
+ * @requires keyboard/map
+ */
+define([
+    'src/keyboard/map'
+], function (keyboardMap) {
+    module('keyboard/map');
+    var tmp = {
+        keyCode: 91,
+        charCode: 91,
+        which: 91
+    };
+
+    test('_getKeyId', function () {
+        equal(Object.prototype.toString.call(keyboardMap._getKeyId), '[object Function]', 'keyboardMap._getKeyId should be a function');
+        equal(keyboardMap._getKeyId(91), 91, 'Should output 91');
+        equal(keyboardMap._getKeyId(tmp), 91, 'Should output 91');
+    });
+
+    test('getKeyName', function () {
+        equal(Object.prototype.toString.call(keyboardMap.getKeyName), '[object Function]', 'keyboardMap.getKeyName should be a function');
+        equal(keyboardMap.getKeyName(91), 'SUPER', 'Key 91 is "SUPER" key (Windows/Apple)');
+    });
+
+    test('isKeyName', function () {
+        equal(Object.prototype.toString.call(keyboardMap.isKeyName), '[object Function]', 'keyboardMap.isKeyName should be a function');
+        equal(keyboardMap.isKeyName(tmp, 'super'), true, 'Key pressed is "SUPER" key (Windows/Apple)');
+    });
+
+    test('isOneOf', function () {
+        equal(Object.prototype.toString.call(keyboardMap.isOneOf), '[object Function]', 'keyboardMap.isOneOf should be a function');
+        equal(keyboardMap.isOneOf(tmp, ['super', 'space', 'backspace']), true, 'Key pressed is either "SUPER", "SPACE" or "BACKSPACE" key');
+    });
+});
