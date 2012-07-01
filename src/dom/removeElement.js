f0d6d2418c74ed7ecac3e011c1084195266794e6
Rob Taylor <manix84@gmail.com>
2012-07-01 16:53:42 +0100

    Relocating dom/removeElement references.

--- src/dom/removeElement.js
+++ src/dom/removeElement.js
@@ -1,12 +1,13 @@
 /**
  * @author Rob Taylor [manix84@gmail.com]
  */
-define('utils/dom/removeElement', function () {
+define('dom/removeElement', function () {
 
     var trash = document.createElement("div"),
         /**
          * Remove element from the dom.
-         * @exports utils/dom/removeElement
+         * @exports dom/removeElement
+         *
          * @param {HMTLElement} element - Dom element object you wish to remove.
          */
         removeElement = function (element) {
