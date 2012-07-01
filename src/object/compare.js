36fb2899bda31ec43e7b8f5a3b21854a78203865
Rob Taylor <manix84@gmail.com>
2012-07-01 16:59:23 +0100

    Cleaning up comments and adding JSON alternative to for loop.

--- src/object/compare.js
+++ src/object/compare.js
@@ -1,57 +1,62 @@
 /**
  * @author Rob Taylor [manix84@gmail.com]
  */
-define('utils/object/compare', function () {
+define('object/compare', function () {
     /**
      * Compare two objects to each-other.
-     * @param {object} objectA - first object to be compared to second
-     * @param {object} objectB - second object to be compared to first
-     * @example object.compare({key1: 'var', key2: 'var'}, {key1: 'var', key2: 'var'}); // RETURNS: true
-     * @example object.compare({key1: 'var', key2: 'var'}, {key1: 'var'}); // RETURNS: false
-     * @returns {boolean}
+     * @exports object/compare
+     *
+     * @param {Object} objectA - first object to be compared to second
+     * @param {Object} objectB - second object to be compared to first
+     * @example compare({key1: 'var', key2: 'var'}, {key1: 'var', key2: 'var'}); // RETURNS: true
+     * @example compare({key1: 'var', key2: 'var'}, {key1: 'var'}); // RETURNS: false
+     * @returns {Boolean}
      */
     var compare = function (objectA, objectB) {
         var key;
 
-        if (typeof objectA !== 'object' || typeof objectB !== 'object') {
-            return false;
-        } else if (this.length(objectA) !== this.length(objectB)) {
+        if (Object.prototype.toString.call(objectA) !== '[object Object]' ||
+            Object.prototype.toString.call(objectB) !== '[object Object]') {
             return false;
         } else {
-            for (key in objectA) {
-                if (objectA.hasOwnProperty(key)) {
-                    if (typeof objectA[key] !== typeof objectB[key]) {
-                        return false;
-                    }
-                    if ((objectA[key] === null) !== (objectB[key] === null)) {
-                        return false;
-                    }
-                    switch (typeof objectA[key]) {
-                    case 'undefined':
-                        if (typeof objectB[key] !== 'undefined') {
+            if (typeof JSON === 'object' && typeof JSON.stringify !== 'undefined') {
+                return (JSON.stringify(objectA) === JSON.stringify(objectB));
+            } else {
+                for (key in objectA) {
+                    if (objectA.hasOwnProperty(key)) {
+                        if (typeof objectA[key] !== typeof objectB[key]) {
                             return false;
                         }
-                        break;
-                    case 'object':
-                        if (objectA[key] !== null && objectB[key] !== null &&
-                                (objectA[key].constructor.toString() !== objectB[key].constructor.toString() ||
-                                !compare(objectA[key], objectB[key]))) {
+                        if ((objectA[key] === null) !== (objectB[key] === null)) {
                             return false;
                         }
-                        break;
-                    case 'function':
-                        if (objectA[key].toString() !== objectB[key].toString()) {
-                            return false;
-                        }
-                        break;
-                    default:
-                        if (objectA[key] !== objectB[key]) {
-                            return false;
+                        switch (typeof objectA[key]) {
+                        case 'undefined':
+                            if (typeof objectB[key] !== 'undefined') {
+                                return false;
+                            }
+                            break;
+                        case 'object':
+                            if (objectA[key] !== null && objectB[key] !== null &&
+                                    (objectA[key].constructor.toString() !== objectB[key].constructor.toString() ||
+                                    !compare(objectA[key], objectB[key]))) {
+                                return false;
+                            }
+                            break;
+                        case 'function':
+                            if (objectA[key].toString() !== objectB[key].toString()) {
+                                return false;
+                            }
+                            break;
+                        default:
+                            if (objectA[key] !== objectB[key]) {
+                                return false;
+                            }
                         }
                     }
                 }
+                return true;
             }
-            return true;
         }
     };
 
