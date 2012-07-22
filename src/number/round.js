4a6caa53c00d4af5bc282df294b48a745888ead4
Rob Taylor <manix84@gmail.com>
2012-07-22 01:27:21 +0100

    Making the decimal length optional.

--- src/number/round.js
+++ src/number/round.js
@@ -8,10 +8,11 @@ define('number/round', function () {
      * @exports number/round
      *
      * @param  {Number} number - A float that needs rounding.
-     * @param  {Number} decimal - The number of decimal places that you want to keep.
+     * @param  {Number} [decimal] - The number of decimal places that you want to keep.
      * @return {Number} A float with the corrisponding number of decimal places.
      */
     var round = function (number, decimal) {
+        decimal = decimal || 0;
         return Math.round(number * Math.pow(10, decimal)) / Math.pow(10, decimal);
     };
 
