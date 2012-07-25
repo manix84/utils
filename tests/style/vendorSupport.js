ac3f3fd085148d6c41198cedd3a8fba8fd87ec4a
Rob Taylor <manix84@gmail.com>
2012-07-25 21:45:48 +0100

    Adding correct style/vendorSupport test.

--- tests/style/vendorSupport.js
+++ tests/style/vendorSupport.js
@@ -1,25 +1,17 @@
 /**
- * Tests for utils/support
+ * Tests for style/vendorSupport
  * @author Rob Taylor [manix84@gmail.com]
- * @module tests/utils/supportTest
- * @requires utils/support
- * @requires utils/core
+ * @module tests/style/vendorSupport
+ * @requires style/vendorSupport
  */
-define('tests/utils/supportTest', [
-    'utils/support',
-    'utils/core'
-], function (support, utilsBase) {
-    module('Utils.Support');
+define('tests/vendorSupport', [
+    'style/vendorSupport'
+], function (vendorSupport) {
+    module('style');
 
-    test('"Namespaces"', function () {
-        equal(utilsBase.getType(support), 'object', 'Support is a function');
-    });
-
-    test('property', function () {
-        equal(utilsBase.getType(support.cssProperty), 'function', 'Support.Property should be a function');
-
-        notEqual(support.cssProperty('borderRadius'), undefined, 'Support.Property should return in a boolean when used');
-        notEqual(support.cssProperty('textShadow'), undefined, 'Support.Property should return in a boolean when used');
+    test('vendorSupport', function () {
+        notEqual(vendorSupport('borderRadius'), undefined, 'Support.Property should return in a boolean when used');
+        notEqual(vendorSupport('textShadow'), undefined, 'Support.Property should return in a boolean when used');
     });
 
 
