7ad112c46346931a2a809a1b7ce9cd67257ead71
Rob Taylor <manix84@gmail.com>
2012-07-26 00:24:21 +0100

    Adding array/toObject test.

---
+++ tests/array/toObject.js
@@ -0,0 +1,15 @@
+/**
+ * Tests for array/toObject
+ * @author Rob Taylor [manix84@gmail.com]
+ * @requires array/toObject
+ */
+define([
+    'array/toObject'
+], function (toObject) {
+    module('array');
+
+    test('toObject', function () {
+        equal(toObject(['cat', 'dog', 'bird']), {0: 'cat', 1: 'dog', 2: 'bird'}, 'Array should successfully be an object now.');
+        throws(toObject({1: 'cat', 2: 'dog', 3: 'bird'}), 'Object should cause an exception.');
+    });
+});
