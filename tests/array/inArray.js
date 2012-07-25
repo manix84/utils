3575901d63a820c0dd32f9f0de2b9037a9858b1c
Rob Taylor <manix84@gmail.com>
2012-07-25 23:55:49 +0100

    Adding array/inArray test.

---
+++ tests/array/inArray.js
@@ -0,0 +1,16 @@
+/**
+ * Tests for array/inArray
+ * @author Rob Taylor [manix84@gmail.com]
+ * @requires array/inArray
+ */
+define([
+    'array/inArray'
+], function (inArray) {
+    module('array');
+
+    test('inArray', function () {
+        equal(inArray('bird', ['cat', 'dog', 'bird']), true, 'Array item "bird" was found.');
+        equal(inArray('fish', ['cat', 'dog', 'bird']), false, 'Array item "fish" was not found.');
+        throws(inArray('fish', {1: 'cat', 2: 'dog', 3: 'bird'}), 'Object should cause an exception.');
+    });
+});
