a9309fb7f56c0f392bd755aee465c8b992a49bf8
Rob Taylor <manix84@gmail.com>
2012-07-25 21:49:04 +0100

    Adding useragent/browser test.

---
+++ tests/useragent/browser.js
@@ -0,0 +1,23 @@
+/**
+ * Tests for useragent/browser
+ * @author Rob Taylor <manix84@gmail.com>
+ * @module tests/useragent/browser
+ * @requires useragent/browser
+ */
+define('tests/useragent/browser', [
+    'useragent/browser'
+], function (browser) {
+    module('useragent');
+
+    test('browser', function () {
+        equal(Object.prototype.toString.call(browser), '[object Object]', 'browser should be an object');
+
+        notEqual(browser.firefox, undefined, 'browser.firefox should be defined');
+        notEqual(browser.chrome, undefined, 'browser.chrome should be defined');
+        notEqual(browser.safari, undefined, 'browser.Safari should be defined');
+        notEqual(browser.msie, undefined, 'browser.msie should be defined');
+        notEqual(browser.opera, undefined, 'browser.opera should be defined');
+        notEqual(browser.wii, undefined, 'browser.wii should be defined');
+        notEqual(browser.playstation, undefined, 'browser.playstation should be defined');
+    });
+});
