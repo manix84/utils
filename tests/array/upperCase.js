c6215df76789c73f9ce6ababff2c1497a59d7be6
Rob Taylor <manix84@gmail.com>
2012-07-26 00:24:34 +0100

    Adding array/upperCase test.

---
+++ tests/array/upperCase.js
@@ -0,0 +1,15 @@
+/**
+ * Tests for array/upperCase
+ * @author Rob Taylor [manix84@gmail.com]
+ * @requires array/upperCase
+ */
+define([
+    'array/upperCase'
+], function (upperCase) {
+    module('array');
+
+    test('upperCase', function () {
+        equal(upperCase(['Cat', 'dOG', 'BIRD']), ['CAT', 'DOG', 'BIRD'], 'This multicase array, should now be all upper case.');
+        throws(upperCase({1: 'cat', 2: 'dog', 3: 'bird'}), 'Object should cause an exception.');
+    });
+});
