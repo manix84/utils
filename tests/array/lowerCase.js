f5751e9f3d31327627e4d76f7bb788735e922403
Rob Taylor <manix84@gmail.com>
2012-07-26 00:24:05 +0100

    Fixing array/lowerCase test.

--- tests/array/lowerCase.js
+++ tests/array/lowerCase.js
@@ -9,7 +9,7 @@ define([
     module('array');
 
     test('lowerCase', function () {
-        equal(lowerCase(['Cat', 'dOG', 'BIRD']), 2, 'This multicase array, should now be all lower case.');
+        equal(lowerCase(['Cat', 'dOG', 'BIRD']), ['cat', 'dog', 'bird'], 'This multicase array, should now be all lower case.');
         throws(lowerCase({1: 'cat', 2: 'dog', 3: 'bird'}), 'Object should cause an exception.');
     });
 });
