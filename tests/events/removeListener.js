7de12cf4bc13b9023cf555a3c40b1628e6d67c7b
Rob Taylor <manix84@gmail.com>
2012-07-26 21:59:02 +0100

    Adding remove event test.

---
+++ tests/events/removeListener.js
@@ -0,0 +1,33 @@
+/**
+ * Tests for event/addListener
+ * @author Rob Taylor [manix84@gmail.com]
+ * @requires event/addListener
+ */
+define([
+    'events/removeListener',
+    'events/addListener'
+], function (removeListener, addListener) {
+    module('events');
+
+    var linkElem = document.createElement('a');
+    linkElem.setAttribute('href', '#success');
+    linkElem.innerText = ('test');
+    document.getElementById('qunit-fixture').appendChild(linkElem);
+
+    asyncTest('removeListener', function () {
+
+        var removeEventTest = window.setTimeout(function () {
+            ok(true, 'Click event was not heard - Success.');
+            start();
+        }, 100);
+
+        addListener(linkElem, 'click', function (event) {
+            window.clearTimeout(removeEventTest);
+            ok(false, 'Click event heard on test element - Fail.');
+            start();
+        });
+        removeListener(linkElem, 'click');
+
+        linkElem.click();
+    });
+});
